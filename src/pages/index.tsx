119649d4cc4241ed3cce3afd70906da48f8881ef
allyson7 <allysonlopes0@gmail.com>
2022-11-29 18:02:17 -0400

    :sparkles: created home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,25 @@
-import styles from "../styles/home.module.scss";
+import Head from "next/head";
+import styles from "./home.module.scss";
 
 export default function Home() {
-  return <h1 className={styles.title}>Hello ignite</h1>;
+  return (
+    <>
+      <Head>
+        <title>Início | spirit.news</title>
+      </Head>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏Seja bem vindo</span>
+          <h1>
+            Notícias sobre <span>Espiritismo</span>
+          </h1>
+          <p>
+            Tenha acesso as novas publicações <br />
+            <span>por R$ 2,99 por mes</span>
+          </p>
+        </section>
+        <img src="/images/avatar.svg" alt="girl coding" />
+      </main>
+    </>
+  );
 }
