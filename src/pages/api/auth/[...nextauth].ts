2a9db595e8c56b5b18712952231ee1f75d39898c
allyson7 <allysonlopes0@gmail.com>
2022-12-09 18:04:16 -0400

    feat: now, the aplication dont duplicate same emails

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -25,11 +25,18 @@ export const authOptions = {
       try {
         await fauna.query(
           q.If(
-            q.Not(q.Exists(q.Match(
-
-            )))
+            q.Not(
+              q.Exists(
+                q.Match(q.Index("user_by_email"), q.Casefold(user.email))
+              )
+            ),
+            q.Create(q.Collection("users"), { data: { email } }),
+            q.Get(
+              // SElect
+              q.Match(q.Index("user_by_email"), q.Casefold(user.email))
+            )
           )
-          q.Create(q.Collection("users"), { data: { email } }));
+        );
 
         return true;
       } catch {
