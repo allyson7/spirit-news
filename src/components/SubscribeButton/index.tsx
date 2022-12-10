30eb6be0c155df2fcd4e62eb55e8c1bb7446425f
allyson7 <allysonlopes0@gmail.com>
2022-12-10 11:59:43 -0400

    feat: configured checkout function

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,12 +1,31 @@
+import { signIn, useSession } from "next-auth/react";
 import styles from "./styles.module.scss";
 
 interface SubscribeButtonProps {
   priceId: string;
 }
 
+/* lugares seguros pra usar key secreta do stripe
+  getServerSideProps (SSR)
+  getStaticProps (SSG)
+  API routes <-
+*/
+
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const { data: session } = useSession();
+  function handleSubscribe() {
+    if (!session) {
+      signIn("github");
+      return;
+    }
+  }
+
   return (
-    <button type="button" className={styles.subscribeButton}>
+    <button
+      type="button"
+      onClick={handleSubscribe}
+      className={styles.subscribeButton}
+    >
       Inscreva-se agora
     </button>
   );
