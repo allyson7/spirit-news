087534c9b2ff54ee4e918e97a2d02dac9b71dfdd
allyson7 <allysonlopes0@gmail.com>
2022-12-12 18:10:26 -0400

    feat: checkout, stripe customer creation working now

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,5 +1,8 @@
-import { signIn, useSession } from "next-auth/react";
-import styles from "./styles.module.scss";
+import { signIn, useSession } from 'next-auth/react';
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
+
+import styles from './styles.module.scss';
 
 interface SubscribeButtonProps {
   priceId: string;
@@ -13,17 +16,32 @@ interface SubscribeButtonProps {
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
   const { data: session } = useSession();
-  function handleSubscribe() {
+
+  async function handleSubscribe() {
     if (!session) {
-      signIn("github");
+      signIn('github');
       return;
+    }
+
+    // criar checkout session
+    try {
+      const response = await api.post('/subscribe');
+
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJs();
+
+      console.log('id sessão:', sessionId);
+      await stripe.redirectToCheckout({ sessionId: sessionId.id });
+    } catch (err) {
+      alert(err.message);
     }
   }
 
   return (
     <button
       type="button"
-      onClick={handleSubscribe}
+      onClick={() => handleSubscribe()}
       className={styles.subscribeButton}
     >
       Inscreva-se agora
